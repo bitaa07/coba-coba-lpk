987a62419c16a01bedff000d65bc9e200504310c
bitaa07 <tsabitafadhillah194@gmail.com>
2025-07-15 17:36:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,7 @@ def get_hazard_symbol(bahaya):
 
 st.set_page_config(page_title="Senyawa Kimia Organik Berbahaya", layout="centered")
 st.title("🧪 Daftar Senyawa Kimia Organik Berbahaya")
-
-    st.markdown(f"""
+st.markdown(f"""
     ## 🧪 {row['Senyawa']}
     - **Rumus Molekul:** {row['Rumus Molekul']}
     - **Bahaya:** {get_hazard_symbol(row['Bahaya'])} {row['Bahaya']}
